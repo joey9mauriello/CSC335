3e81230cb6043f6a777e14e95b58861708ca7608
joey9mauriello <125524713+joey9mauriello@users.noreply.github.com>
2023-02-15 20:35:32 -0700

    Update GenericSet.java

--- PA 2/GenericSet.java
+++ PA 2/GenericSet.java
@@ -12,10 +12,22 @@ public class GenericSet<T> {
 
 	private ArrayList<T> list = new ArrayList<>();
 
+	/**
+	* This gets the element at index i.
+	* 
+	* @param i An integer of an index
+	* @return The element at index i
+	*/
 	public T get(int i) {
 		return list.get(i);
 	}
 
+	/**
+	* This adds a value to the set.
+	* 
+	* @param value The generic value to be added
+	* @return A boolean based on if the value was succesfully added
+	*/
 	public boolean add(T value) {
 		if (list.contains(value)) {
 			return false;
@@ -24,10 +36,21 @@ public class GenericSet<T> {
 		return true;
 	}
 
+	/**
+	* This gets the size of the set.
+	* 
+	* @return An integer of the size of the set
+	*/
 	public int size() {
 		return list.size();
 	}
 
+	/**
+	* This determines if the set contains a specific value.
+	* 
+	* @param value The generic value to be checked in the set
+	* @return A boolean based on if the set contains the value
+	*/
 	public boolean contains(T value) {
 		if (list.contains(value)) {
 			return true;
@@ -35,6 +58,10 @@ public class GenericSet<T> {
 		return false;
 	}
 
+	/**
+	* This unionizes the current set with another set.
+	* 
+	* @param list2 The s
 	public GenericSet<T> unionWith(GenericSet<T> list2) {
 		GenericSet<T> finalSet = this;
 		for (int i = 0; i < list2.size()-1; i++) {
